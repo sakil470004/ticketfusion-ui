0f84d9699a29f3f3f21cf36b7b431cf001d92760
Mynul Islam <mynul.sakil@gmail.com>
2024-06-14 21:03:28 +0600

    fix scroll problem

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,15 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 
 const MainLayout = () => {
+  const { pathname } = useLocation();
+  useEffect(()=>{
+    window.scrollTo(0, 0);
+
+  },[pathname])
   return (
     <div className="max-w-screen-xl mx-auto">
       <Navbar />
